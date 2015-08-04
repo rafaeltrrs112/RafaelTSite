f35801e267d066c5bda60dc80b65f9a52a61414f
Rafael O. Torres <rafaeltrrs112@gmail.com>
2015-08-04 02:59:39 -0400

    Finished nav-bar basic functionality. Next is creating the coin-card.

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,36 +1,20 @@
 ///<reference path="TouchEvent.d.ts"/>
 ///<reference path="cust_library.ts"/>
-//Touch events for the mobile portions of the site.
+//Touch events for the mobile portions of the site creates four event listeners.
 window.addEventListener('load', function () {
     //Comment out this code to ensure that you understand what is going on.
-    var box1 = document.getElementById('nav-button');
-    var box2 = document.getElementById('content-box');
-    var statusdiv = document.getElementById('statusdiv');
-    var startx = 0;
-    box1.addEventListener('touchstart', function (e) {
+    var nav_button = document.getElementById('nav-button');
+    var body_box = document.getElementById('content-box');
+    //Event listener for the navButton to toggle the nav.
+    nav_button.addEventListener('touchstart', function (e) {
         var touchobj = e.changedTouches[0]; // reference first touch point (ie: first finger)
-        startx = parseInt(touchobj.clientX.toString()); // get x position of touch point relative to left edge of browser
-        console.log('Status: touchstart ClientX: ' + startx + 'px');
-        //e.preventDefault();
+        e.preventDefault();
         toggleNav();
     }, false);
-    box2.addEventListener('touchstart', function (e) {
+    //Event listener for the body to disableNav() if nav is active and the user taps the body.
+    body_box.addEventListener('touchstart', function (e) {
         var touchobj = e.changedTouches[0]; // reference first touch point (ie: first finger)
-        startx = parseInt(touchobj.clientX.toString()); // get x position of touch point relative to left edge of browser
         e.preventDefault();
-        //
         bodyScope();
-    }, false);
-    box1.addEventListener('touchmove', function (e) {
-        var touchobj = e.changedTouches[0]; // reference first touch point for this event
-        var dist = parseInt(touchobj.clientX.toString()) - startx;
-        console.log('Status: touchmove \n Horizontal distance traveled: ' + dist + 'px');
-        e.preventDefault(); //This prevents the event from doing the action it normally would. In this case
-        //touching and moving (swiping) would not scroll.
-    }, false);
-    box1.addEventListener('touchend', function (e) {
-        var touchobj = e.changedTouches[0]; // reference first touch point for this event
-        console.log('Status: touchend \n Resting x coordinate: ' + touchobj.clientX + 'px');
-        e.preventDefault();
     }, false);
 }, false);
--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,42 +1,24 @@
 ///<reference path="TouchEvent.d.ts"/>
 ///<reference path="cust_library.ts"/>
-//Touch events for the mobile portions of the site.
+//Touch events for the mobile portions of the site creates four event listeners.
 window.addEventListener('load', function(){
-        //Comment out this code to ensure that you understand what is going on.
-        var box1 : HTMLElement = document.getElementById('nav-button');
-        var box2 : HTMLElement = document.getElementById('content-box');
-        var statusdiv : HTMLElement = document.getElementById('statusdiv');
-        var startx = 0;
 
-        box1.addEventListener('touchstart', function(e : TouchEvent){
-            var touchobj = e.changedTouches[0] ;// reference first touch point (ie: first finger)
-            startx = parseInt(touchobj.clientX.toString()) ;// get x position of touch point relative to left edge of browser
-            console.log('Status: touchstart ClientX: ' + startx + 'px');
-            //e.preventDefault();
-            toggleNav();
-        }, false);
+    //Comment out this code to ensure that you understand what is going on.
+    var nav_button : HTMLElement = document.getElementById('nav-button');
+    var body_box : HTMLElement = document.getElementById('content-box');
 
-        box2.addEventListener('touchstart', function(e : TouchEvent){
-            var touchobj = e.changedTouches[0] ;// reference first touch point (ie: first finger)
-            startx = parseInt(touchobj.clientX.toString()) ;// get x position of touch point relative to left edge of browser
-            e.preventDefault();
-            //
-            bodyScope();
-        }, false);
-
-        box1.addEventListener('touchmove', function(e: TouchEvent){
-            var touchobj = e.changedTouches[0] ;// reference first touch point for this event
-            var dist = parseInt(touchobj.clientX.toString()) - startx;
-            console.log('Status: touchmove \n Horizontal distance traveled: ' + dist + 'px');
-            e.preventDefault(); //This prevents the event from doing the action it normally would. In this case
-                                //touching and moving (swiping) would not scroll.
-        }, false);
-
-        box1.addEventListener('touchend', function(e: TouchEvent){
-            var touchobj = e.changedTouches[0] ;// reference first touch point for this event
-            console.log('Status: touchend \n Resting x coordinate: ' + touchobj.clientX + 'px');
-            e.preventDefault()
-        }, false)
-
+    //Event listener for the navButton to toggle the nav.
+    nav_button.addEventListener('touchstart', function(e : TouchEvent){
+        var touchobj = e.changedTouches[0] ;// reference first touch point (ie: first finger)
+        e.preventDefault();
+        toggleNav();
     }, false);
+
+    //Event listener for the body to disableNav() if nav is active and the user taps the body.
+    body_box.addEventListener('touchstart', function(e : TouchEvent){
+        var touchobj = e.changedTouches[0] ;// reference first touch point (ie: first finger)
+        e.preventDefault();
+        bodyScope();
+    }, false);
+}, false);
 
